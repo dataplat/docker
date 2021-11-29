80a7bac1002458e7e07fcca7c869df4490d3f334
Chrissy LeMaire <clemaire@gmail.com>
2021-11-29 15:22:37 +0100

    add back sqlcmd

--- sqlinstance/scripts/start-sql.sh
+++ sqlinstance/scripts/start-sql.sh
@@ -6,6 +6,13 @@ export PATH=$PATH:/opt/mssql-tools/bin
 # set the configs
 cp /tmp/mssql.conf /var/opt/mssql/mssql.conf
 
+# check for arm64 which does not support sqlcmd
+arch=$(lscpu | awk '/Architecture:/{print $2}')
+if [ "$arch" = "aarch64" ]; then
+    mkdir /opt/mssql-tools /opt/mssql-tools/bin
+    cp /tmp/sqlcmd /opt/mssql-tools/bin
+fi
+
 # startup, wait for it to finish starting
 # then run the setup script
 /opt/mssql/bin/sqlservr & sleep 20 & /tmp/configure.sh
