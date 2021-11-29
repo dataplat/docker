9884a556b9df22aab096dac9e770cf266ac35c11
Chrissy LeMaire <clemaire@gmail.com>
2021-11-29 01:09:04 +0100

    Update initial-start.sh

--- scripts/initial-start.sh
+++ scripts/initial-start.sh
@@ -6,15 +6,14 @@ export PATH=$PATH:/opt/mssql-tools/bin
 # set the configs
 cp /tmp/mssql.conf /var/opt/mssql/mssql.conf
 
-# check for arm64 which does not support sqlcmd
+# check for arm64 which needs a different sqlcmd
 arch=$(lscpu | awk '/Architecture:/{print $2}')
-
-# startup, wait for it to finish starting
-# then run the setup script
 
 if [ "$arch" = "aarch64" ]; then
     mkdir /opt/mssql-tools /opt/mssql-tools/bin
     cp /tmp/sqlcmd /opt/mssql-tools/bin
 fi
 
+# startup, wait for it to finish starting
+# then run the setup script
 /opt/mssql/bin/sqlservr & sleep 10 & /tmp/setup.sh
