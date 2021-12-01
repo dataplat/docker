98bb50717b98d034ccbe0974392d80d85425b733
Chrissy LeMaire <clemaire@gmail.com>
2021-12-01 11:14:00 +0100

    prettier

--- sqlinstance/scripts/configure.sh
+++ sqlinstance/scripts/configure.sh
@@ -19,8 +19,8 @@ sqlcmd -d master -Q "EXEC sp_dropserver @@SERVERNAME"
 # if it's the primary server, restore pubs and northwind and create a bunch of objects
 if [ -f "/tmp/primary" ]; then
     sqlcmd -S localhost -d master -Q "EXEC sp_addserver 'mssql1', local"
-    # Download instead of including it in the repo -- it reduces the size of the context
-    # and makes the secondary image smaller
+    # Download instead of including it in the repo -- it reduces 
+    # the size of the context and makes the secondary image smaller
     wget https://github.com/sqlcollaborative/docker/raw/a61d8e1ffb150cae767c27737ad07e730d4e76dd/sqlinstance/sql/northwind.bak
     wget https://github.com/sqlcollaborative/docker/raw/a61d8e1ffb150cae767c27737ad07e730d4e76dd/sqlinstance/sql/pubs.bak
     sqlcmd -S localhost -d master -i /tmp/restore-db.sql
