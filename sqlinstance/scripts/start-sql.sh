842b9a1e665901d179fea20e4bf35a0379d9fb50
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-12-03 01:51:56 +0100

    add sqlcmd from releases (#7)

--- sqlinstance/scripts/start-sql.sh
+++ sqlinstance/scripts/start-sql.sh
@@ -9,9 +9,11 @@ cp /tmp/mssql.conf /var/opt/mssql/mssql.conf
 # check for arm64 which does not support sqlcmd
 arch=$(lscpu | awk '/Architecture:/{print $2}')
 if [ "$arch" = "aarch64" ]; then
+    wget https://github.com/microsoft/go-sqlcmd/releases/download/v0.2.0/sqlcmd-v0.2.0-linux-arm64.tar.bz2
+    tar -xvf sqlcmd-v0.2.0-linux-arm64.tar.bz2
     mkdir /opt/mssql-tools /opt/mssql-tools/bin
-    cp /tmp/sqlcmd /opt/mssql-tools/bin
-    chmod +x /opt/mssql-tools/bin/sqlcmd
+    cp sqlcmd /opt/mssql-tools/bin
+    chmod +x /opt/mssql-tools/bin/sqlcmd   
 fi
 
 # startup, wait for it to finish starting
