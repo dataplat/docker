f03857d35f2d5fa7b9bc1ffd31be5e4f9d841d25
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-12-04 13:58:09 +0100

    Fix jobs SQL & remove whitespace (#10)

--- sqlinstance/scripts/start-sql.sh
+++ sqlinstance/scripts/start-sql.sh
@@ -13,7 +13,7 @@ if [ "$arch" = "aarch64" ]; then
     tar -xvf sqlcmd-v0.2.0-linux-arm64.tar.bz2
     mkdir /opt/mssql-tools /opt/mssql-tools/bin
     cp sqlcmd /opt/mssql-tools/bin
-    chmod +x /opt/mssql-tools/bin/sqlcmd   
+    chmod +x /opt/mssql-tools/bin/sqlcmd
 fi
 
 # startup, wait for it to finish starting
