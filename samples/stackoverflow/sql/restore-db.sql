eaeee8f1e2c8cd3067ddb5652cf9a625d0479f5a
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2022-11-04 08:00:22 +0100

    Fix broken database owner for Northwind (#21)

--- samples/stackoverflow/sql/restore-db.sql
+++ samples/stackoverflow/sql/restore-db.sql
@@ -7,3 +7,9 @@ USE [master]
 RESTORE DATABASE [pubs] FROM  DISK = N'/tmp/pubs.bak' WITH  FILE = 1,  MOVE N'pubs' TO N'/var/opt/mssql/data/pubs.mdf',  MOVE N'pubs_log' TO N'/var/opt/mssql/data/pubs_log.ldf',  NOUNLOAD,  STATS = 5
 
 GO
+
+USE [master]
+ALTER AUTHORIZATION ON DATABASE::[Northwind] TO [sqladmin]
+ALTER AUTHORIZATION ON DATABASE::[pubs] TO [sqladmin]
+
+GO
